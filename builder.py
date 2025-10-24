843710479d367c60b435a7068bed7dda171da935
Stenio Silveira <stenio.silveira@softplan.com.br>
2025-10-24 16:10:50 -0300

    fix: add strip_jsonc_comments

--- builder.py
+++ builder.py
@@ -11,10 +11,23 @@ def load_jsonc(path):
     with open(path, encoding="utf-8") as f:
         lines = []
         for line in f:
-            # Remove // comments
-            line = line.split('//')[0]
+            line = strip_jsonc_comments(line)
             lines.append(line)
         return json.loads(''.join(lines))
+
+
+def strip_jsonc_comments(line):
+    in_string = False
+    result = ''
+    i = 0
+    while i < len(line):
+        if line[i] == '"' and (i == 0 or line[i-1] != '\\'):
+            in_string = not in_string
+        if not in_string and line[i:i+2] == '//':
+            break
+        result += line[i]
+        i += 1
+    return result
 
 
 # Load builder config (JSONC)
@@ -34,7 +47,7 @@ def collect_builder_modules():
         pkgs.update(module_src.get("packages", []))
         cmds.update(module_src.get("commands", []))
 
-    return  sorted(aur_pkgs), sorted(pkgs), sorted(cmds)
+    return  sorted(aur_pkgs), sorted(pkgs), cmds
 
 
 def main():
