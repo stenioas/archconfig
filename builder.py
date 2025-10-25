5fad84dee462280d39d912414350c26fb5f77b6d
Stenio Silveira <stenio.silveira@softplan.com.br>
2025-10-25 15:42:16 -0300

    feat: add error handler in builder

--- builder.py
+++ builder.py
@@ -71,4 +71,8 @@ def main():
         print("Usage: builder.py --list <packages|commands> or -l <packages|commands>")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
