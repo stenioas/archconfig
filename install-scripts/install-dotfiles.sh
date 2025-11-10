4baf7001e26f57f297142e89de6cce4e83531423
Stenio Silveira <stenioas@gmail.com>
2025-11-10 20:40:14 -0300

    refactor(install-dotfiles): adjust dotfiles script

--- install-scripts/install-dotfiles.sh
+++ install-scripts/install-dotfiles.sh
@@ -1,12 +1,14 @@
 #!usr/bin/env bash
 # ----------------------------------------------------------------------------
 
+TARGET_DIR="${HOME}/git/github/dotfiles"
+
 # Install dotfiles
-if [[ -d ${HOME}/.dotfiles ]]; then
-  echo "Dotfiles already installed."
-  exit 0
-else
-  echo "Cloning and installing dotfiles..."
-  git clone https://github.com/stenioas/dotfiles.git ${HOME}/.dotfiles
-  bash ${HOME}/.dotfiles/install_dotfiles.sh
+if [[ -d ${TARGET_DIR} ]]; then
+  echo "Dotfiles folder already exists. A backup will be created in ${TARGET_DIR}_old_$(date +%Y%m%d%H%M%S)."
+  mv ${TARGET_DIR} "${TARGET_DIR}_old_$(date +%Y%m%d%H%M%S)"
 fi
+
+echo "Cloning and installing dotfiles..."
+git clone https://github.com/stenioas/dotfiles.git ${TARGET_DIR}
+bash ${TARGET_DIR}/install_dotfiles.sh
